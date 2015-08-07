b3d2c4f5f8271fb2442b2bd11da6a0fd5238aaae
Mark Beech <m@rkbee.ch>
2015-08-07 10:40:20 -0400

    Applied fixes from StyleCI

--- tests/UATests.php
+++ tests/UATests.php
@@ -5,49 +5,49 @@ use Orchestra\Testbench\TestCase;
 
 class UATests extends TestCase
 {
-	protected $LaravelCrawlerDetect;
+    protected $LaravelCrawlerDetect;
 
-	protected function getPackageProviders($app)
-	{
-		return ['Jaybizzle\LaravelCrawlerDetect\LaravelCrawlerDetectServiceProvider'];
-	}
+    protected function getPackageProviders($app)
+    {
+        return ['Jaybizzle\LaravelCrawlerDetect\LaravelCrawlerDetectServiceProvider'];
+    }
 
-	protected function getPackageAliases($app)
-	{
-	    return [
-	        'Crawler' => 'Jaybizzle\LaravelCrawlerDetect\Facades\LaravelCrawlerDetect'
-	    ];
-	}
+    protected function getPackageAliases($app)
+    {
+        return [
+            'Crawler' => 'Jaybizzle\LaravelCrawlerDetect\Facades\LaravelCrawlerDetect',
+        ];
+    }
 
-	public function setUp()
-	{
-		parent::setUp();
-		$this->LaravelCrawlerDetect = new CrawlerDetect;
-	}
+    public function setUp()
+    {
+        parent::setUp();
+        $this->LaravelCrawlerDetect = new CrawlerDetect();
+    }
 
-	protected function getEnvironmentSetUp($app)
-	{
-		// reset base path to point to our package's src directory
-		$app['path.base'] = __DIR__ . '/../src';
-	}
+    protected function getEnvironmentSetUp($app)
+    {
+        // reset base path to point to our package's src directory
+        $app['path.base'] = __DIR__.'/../src';
+    }
 
-	public function testBots()
-	{
-		$lines = file(__DIR__ . '/crawlers.txt');
+    public function testBots()
+    {
+        $lines = file(__DIR__.'/crawlers.txt');
 
-		foreach($lines as $line) {
-			$test = Crawler::isCrawler($line);
-			$this->assertEquals($test, true, $line);
-		}
-	}
+        foreach ($lines as $line) {
+            $test = Crawler::isCrawler($line);
+            $this->assertEquals($test, true, $line);
+        }
+    }
 
-	public function testDevices()
-	{
-		$lines = file(__DIR__ . '/devices.txt');
+    public function testDevices()
+    {
+        $lines = file(__DIR__.'/devices.txt');
 
-		foreach($lines as $line) {
-			$test = Crawler::isCrawler($line);
-			$this->assertEquals($test, false, $line);
-		}
-	}
+        foreach ($lines as $line) {
+            $test = Crawler::isCrawler($line);
+            $this->assertEquals($test, false, $line);
+        }
+    }
 }
